28fedb2e38e8cb4347fcf7b193b99891d2202fcc
luanfv <luanfrancavieira@gmail.com>
2022-07-27 18:14:25 -0300

    feat: add pipes

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,9 +1,32 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 class User {
   id: number;
+
+  @IsNotEmpty({
+    message: 'É necessario informar o nome!',
+  })
+  @IsString()
   name: string;
+
+  @IsEmail(
+    {},
+    {
+      message: 'E-mail inválido!',
+    },
+  )
   email: string;
+
+  @IsNotEmpty({
+    message: 'É necessario informar uma senha!',
+  })
   password: string;
+
+  @IsNotEmpty({
+    message: 'É necessario informar o nome completo!',
+  })
   fullName: string;
+
   createdAt: string;
 }
 
