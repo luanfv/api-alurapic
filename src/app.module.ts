a78b40369df3473cdaee795369a14ed27fcf2ef5
luanfv <luanfrancavieira@gmail.com>
2022-08-19 09:19:10 -0300

    feat: add interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 class AppModule {}
 
